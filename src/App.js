c797e486cae852817355f19f6c3616b5a724feaa
mila2999 <mila.savonik@gmail.com>
2023-08-22 19:45:26 +0300

    components composition

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ function App() {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2023, 7, 13),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'Iphone', amount: 799.49, date: new Date(2023, 2, 15) },
     {
       id: 'e3',
-      title: 'Car Insurance',
+      title: 'Corcinet',
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2023, 2, 23),
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(2021, 5, 12),
+      date: new Date(2023, 5, 10),
     },
   ];
 
