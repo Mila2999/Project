83ec090f3037f5f5d41339fe2cb867dc2584c366
mila2999 <mila.savonik@gmail.com>
2023-08-22 20:19:50 +0300

    Organazing components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Expenses from './components/Expenses';
+// import React from 'react';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
@@ -23,6 +24,12 @@ function App() {
     },
   ];
 
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
   return (
     <div>
       <h2>Let's get started!</h2>
