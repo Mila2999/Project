392d0132f13635e31023a19d7710503bbb20736d
mila2999 <mila.savonik@gmail.com>
2023-10-03 15:46:57 +0300

    Derived State

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,15 +4,25 @@ import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2019');
-  const filterChangeHandler = (selectYear) => {
-    console.log('Expenses.js');
-    setFilteredYear(selectYear);
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredInfoText, setFilteredInfoText] = useState('2019, 2021, 2022');
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    if (selectedYear === '2019') {
+      setFilteredInfoText('2020, 2021, 2022');
+    } else if (selectedYear === '2020') {
+      setFilteredInfoText('2019, 2021, 2022');
+    } else if (selectedYear === '2021') {
+      setFilteredInfoText('2019, 2020, 2022');
+    } else {
+      setFilteredInfoText('2019, 2020, 2021');
+    }
   };
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <p>Data for years {filteredInfoText} is hidden</p>
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
